d471e80c55a06a739412b20dff33a545654f0c16
Rindou <rriinnddoouu@gmail.com>
2019-01-18 18:03:29 +0900

    Update snow-monkey-design-skin-rn-color.php

--- snow-monkey-design-skin-rn-color.php
+++ snow-monkey-design-skin-rn-color.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: リンドウ・カラー
  * Description: 竜胆（りんどう）Webデザインが作成したSnow Monkey用のデザインスキン
- * Version: 0.1.0
+ * Version: 0.1.1
  * @author 古川恵子
  * @license GPL-2.0+
  */
@@ -17,8 +17,14 @@ add_action( 'plugins_loaded', function() {
 	} );
 } );
 
-add_action( 'after_setup_theme', function() {
-	if ( class_exists( '\Snow_Monkey\app\model\Design_Skin' ) ) {
-		new \Snow_Monkey\app\model\Design_Skin( __FILE__ );
+add_action(
+	'after_setup_theme',
+	function() {
+		if ( class_exists( '\Snow_Monkey\app\model\Design_Skin' ) ) {
+			new \Snow_Monkey\app\model\Design_Skin( __FILE__ );
+		}
+		if ( class_exists( '\Framework\Model\Design_Skin' ) ) {
+			new \Framework\Model\Design_Skin( __FILE__ );
+		}
 	}
-} );
+);
